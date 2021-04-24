4c91c4d67b672ad2e49ba2ce1c97096bb1fc1e7b
Milan Wikarski <milan@wikarski.sk>
2021-04-24 20:52:39 +0200

    colors in tests

--- bin/run-tests.sh
+++ bin/run-tests.sh
@@ -5,28 +5,29 @@ cd "$( dirname "$( realpath "$0" )" )/.."
 python3 src/test.py || exit 1
 
 echo ''
-echo 'Running examples...'
+echo 'Examples'
 echo ''
 
 for example in $( find ./examples -name '*.py' ) ; do
   example=${example#.\/examples\/}
   example=${example%\.py}
 
-  printf "  $example -> "
+  printf " - $example\t"
 
   res="$( ./bin/run-example.sh "$example" | jq -r .res )"
 
-  echo "$res"
-
-  if [ "$res" != 'success' ] ; then
+  if [ "$res" = 'success' ] ; then
+    printf "\033[1;32mOK\033[0m\n"
+  else
+    printf "\033[1;31m$res\033[0m\n"
     exit 1
   fi
 
   if [ "$?" != '0' ] ; then
-    echo "Response code: $?. Aborting..."
+    printf "\033[1;31mResponse code: $?\033[0m\n"
     exit "$?"
   fi
 done
 
 echo ''
-echo 'ALL TESTS PASSED'
+printf "\033[1;32mALL TESTS PASSED\033[0m\n"
