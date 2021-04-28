81e9346975528d175493310aa50ddc80efefbcb5
Milan Wikarski <milan@wikarski.sk>
2021-04-28 07:30:14 +0200

    Docstrings

--- src/utils/random.py
+++ src/utils/random.py
@@ -6,7 +6,7 @@ import binascii
 import os
 from random import random, randint, choice
 import itertools
-from engine.color import COLOR_NAMES
+from engine.color import Color, COLOR_NAMES
 
 
 def random_name(n_bytes=16):
@@ -83,8 +83,13 @@ def random_color_hex():
     - 6: long, no alpha channel
     - 8: long, with alpha channel
 
-  Upper or lower case are both valid.
-  Whitespace padding is valid.
+  Other validity conditions are:
+    - upper or lower case are both valid
+    - whitespace padding is valid
+
+  The probabilities are:
+    1. 50%: use upper case instead of lower case
+    2. 50%: pad the color with random amount of whitespace
 
   returns:
     - color (str): color in hex format
@@ -102,16 +107,26 @@ def random_color_hex():
 
 def random_color_rgba():
   """
-  Returns a random valid color in rgba format
+  Returns a random valid color in rgba format. This means:
+    - both rgb and rgba prefixes are valid
+    - whitespace before and after brackets, commas and whole color is valid
+    - int {0, ..., 255} or percents [0%, 100%] can be used, but not mixed
+    - alpha channel may or may not be specified (default alpha is 1)
 
-  todo: finish this docstring
+  The probabilities are:
+    1. 50%: percentage notation will be used
+    2. 50%: alpha chanell will be specified
+    3. 50%: percentage will be used to specify the alpha channel (given 2.)
+    4. 25%: color part will be padded with random whitespace (for every part)
+    5. 50%: rgba will be used instead of rgb
+    6. 50%: color will be padded with random whitespace
 
   returns:
     - color (str): color in rgba format
   """
-  use_perc = random() > 0.5
-  use_alpha = random() > 0.5
-  use_alpha_perc = use_alpha and random() > 0.5
+  use_perc = random() > 0.5 # 1.
+  use_alpha = random() > 0.5 # 2.
+  use_alpha_perc = use_alpha and random() > 0.5 # 3.
 
   random_int = lambda: str(randint(0, 255))
   random_perc = lambda: f'{random() * 100}%'
@@ -123,12 +138,35 @@ def random_color_rgba():
     rgba.append(random_perc() if use_alpha_perc else random_float())
 
   for i, c in enumerate(rgba):
-    if random() > 0.25:
+    if random() > 0.25: # 4.
       rgba[i] = random_whitespace_pad(c)
 
-  rgba = 'rgb{}({})'.format(choice(['a', '']), ','.join(rgba))
+  rgba = 'rgb{}({})'.format(choice(['a', '']), ','.join(rgba)) # 5.
 
-  if random() > 0.5:
+  if random() > 0.5: # 6.
     rgba = random_whitespace_pad(rgba)
 
   return rgba
+
+
+def random_color():
+  """
+  Returns a random valid color in any format. There is also a 50% probability
+  that an instance of Color will be returned instead of string.
+
+  The probabilities are:
+    1. 1/n: choice of color format
+    2. 50%: color will be used to construct an instance of Color
+
+  returns:
+    - color (str|Color): random color
+  """
+  method = choice([random_color_name, random_color_hex, random_color_rgba])
+  use_class = random() > 0.5
+
+  color = method()
+
+  if use_class:
+    color = Color(color)
+
+  return color
