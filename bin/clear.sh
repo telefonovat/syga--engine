df09422489d9c964a9ce7bf5ad382e0108bd92e8
Milan Wikarski <milan@wikarski.sk>
2021-05-18 16:12:21 +0200

    Clear .json output files in the clear script

--- bin/clear.sh
+++ bin/clear.sh
@@ -4,8 +4,9 @@ cd "$( dirname "$( realpath "$0" )" )/.."
 
 py3clean .
 
-ls logs/algs/ | grep -qPo '\.log$' && rm logs/algs/*.log
-ls src/__algs/ | grep -qPo '\.py$' && rm src/__algs/*.py
+ls ./logs/algs/ | grep -qPo '\.log$' && rm ./logs/algs/*.log
+ls ./src/__algs/ | grep -qPo '\.py$' && rm ./src/__algs/*.py
+ls ./out/ | grep -qPo '\.json$' && rm ./out/*.json
 
 printf '' > logs/debug.log
 
