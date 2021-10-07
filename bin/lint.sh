74937fe117d7aaa3c79910de95a00af884da2109
Milan Wikarski <milan@wikarski.sk>
2021-10-07 20:47:13 +0200

    Bug fix in the lint script

--- bin/lint.sh
+++ bin/lint.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-cd "$( dirname "$( realpath "$0" )" )/../src"
+# Root directory
+cd "$( dirname "$( realpath "$0" )" )/.." || exit 1
 
+# Main
 python3 -m pylint \
   --output-format=text \
-  $(find -not -path "./__algs/*" -type f -name "*.py" ! -path "**/.venv/**")
+  $(find ./src -not -path "./src/__algs/*" -type f -name "*.py" ! -path "**/.venv/**") || exit "$?"
 
-exit "$?"
+# find ./bin/ -name '*.sh' | xargs shellcheck || exit "$?"
+
+exit 0
