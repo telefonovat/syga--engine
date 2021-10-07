019c44e1e590031a8ef0f77a002745a2cdbd56da
Milan Wikarski <milan@wikarski.sk>
2021-10-07 20:52:23 +0200

    Added comments to push script

--- bin/push.sh
+++ bin/push.sh
@@ -3,12 +3,13 @@
 # Root directory
 cd "$( dirname "$( realpath "$0" )" )/.." || exit 1
 
-# Main
+# Parameters
 if [ "$1" = '' ] ; then
   echo "Missing commit message. Aborting..."
   exit 1
 fi
 
+# Main
 git add . || exit "$?"
 git commit -m "$1" || exit "$?"
 git push origin master || exit "$?"
