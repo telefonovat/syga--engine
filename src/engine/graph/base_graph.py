bc29df4b85e317ec9a8f7581ffe7915d1717b42a
Milan Wikarski <milan@wikarski.sk>
2021-12-07 08:40:12 +0100

    Graph Visualizer divided into Graph and DiGraph - both sare a common ancestor BaseGraph

---
+++ src/engine/graph/base_graph.py
@@ -0,0 +1,211 @@
+"""
+The BaseGraph Visualizer module
+"""
+
+from engine.visualizer import Visualizer
+from .graph_colorizer import GraphNodeColorizer, GraphEdgeColorizer
+from .graph_shaper import GraphNodeShaper, GraphEdgeShaper
+from .graph_labeler import GraphEdgeLabeler, GraphNodeLabeler
+
+
+class BaseGraph(Visualizer):
+  """
+  Base Graph is the base class for the following classes:
+    - Graph
+    - DiGraph
+
+  The style properties are as follows:
+    - node color
+    - node shape
+    - node scale
+    - node label
+    - edge color
+    - edge shape
+    - edge scale
+    - edge label
+
+  The stylization of every property can be specified by a designated method.
+  This method simply passes all arguments to the builder of the designated
+  Stylizer
+  """
+
+  def get_type(self):
+    """
+    Returns the type of the graph.
+    """
+    raise NotImplementedError()
+
+
+  def _engine_tick(self):
+    """
+    If a reference to the engine is stored in this graph component, a tick is
+    created. The type of the tick is TICK_SOURCE_STYLIZER
+    """
+    if self._engine is not None:
+      self._engine.tick(self._engine.TICK_SOURCE_STYLIZER)
+
+
+  def get_transformed_state(self):
+    """
+    Returns the transformed state of the Graph which is composed of
+      - list of nodes
+      - list of edges
+      - dict (element to transformed information) for every style property
+
+    If the graph is empty, None is returned. If all components in a tick are
+    None, the tick can be skipped - it is useless.
+
+    Only the properties which have stylizers specified are included in the
+    transformed dict.
+
+    This method MUST be called every time a tick is being generated.
+
+    returns:
+      - transformed_information (dict): The information used for stylization
+    """
+    if not self.nodes and not self.nodes:
+      return None
+
+    transformed = {}
+
+    for name, stylizer in self._stylizers.items():
+      if stylizer is not None:
+        transformed[name] = stylizer.transform(self)
+
+    return {
+      'nodes': list(self.nodes),
+      'edges': list(self.edges),
+      'transformed': transformed
+    }
+
+
+  def interpret_transformed_state(self):
+    """
+    Calls the interpret method for every Stylizer. This method MUST be called
+    after all ticks were generated.
+    """
+    for stylizer in self._stylizers.values():
+      if stylizer is not None:
+        stylizer.interpret()
+
+
+  def compute_style(self, state):
+    """
+    Computes the style of the graph elements using the transformed information.
+    The only parameter of this method is the dict returned by the method
+    `get_transformed_state`. The engine is responsible for keeping the
+    transformed information and calling this method with it as the argument.
+
+    The transformed state for a property might not be present in the state
+    dict. This happends when ticks are generated BEFORE a stylizer has been
+    specified by the user. In such case the style of the property will NOT be
+    included in the style dict.
+
+    The style dict can also be empty if no stylizers were specified when
+    computing the transformed state. In such case the style dict is empty. If
+    all style dicts in a frame are empty - the frame will be ignored.
+
+    parameters:
+      - state (dict): The dict returned by the `get_transformed_state` method
+
+    returns:
+      - style (dict): The final information used to draw the graph
+    """
+    if state is None:
+      return None
+
+    style = {}
+
+    for name, stylizer in self._stylizers.items():
+      if stylizer is not None and name in state['transformed']:
+        style[name] = stylizer.compute(state['transformed'][name])
+
+    return {
+      'type': self.get_type(),
+      'nodes': state['nodes'],
+      'edges': state['edges'],
+      'style': style
+    }
+
+
+  def color_nodes_by(self, *args, **kwargs):
+    """
+    Creates an instance of GraphNodeColorizer used by this graph
+    """
+    self._stylizers['node_colors'] = GraphNodeColorizer.build(*args, **kwargs)
+
+    self._engine_tick()
+
+
+  def shape_nodes_by(self, *args, **kwargs):
+    """
+    Creates an instance of GraphNodeShaper used by this graph
+    """
+    self._stylizers['node_shapes'] = GraphNodeShaper.build(*args, **kwargs)
+
+    self._engine_tick()
+
+
+  def scale_nodes_by(self, *args, **kwargs):
+    """
+    todo: implement this
+    """
+
+
+  def label_nodes_by(self, *args, **kwargs):
+    """
+    Creates an instance of GraphNodeLabeler used by this graph
+    """
+    self._stylizers['node_labels'] = GraphNodeLabeler.build(*args, **kwargs)
+
+    self._engine_tick()
+
+
+  def color_edges_by(self, *args, **kwargs):
+    """
+    Creates an instance of GraphEdgeColorizer used by this graph
+    """
+    self._stylizers['edge_colors'] = GraphEdgeColorizer.build(*args, **kwargs)
+
+    self._engine_tick()
+
+
+  def shape_edges_by(self, *args, **kwargs):
+    """
+    Creates an instance of GraphEdgeShaper used by this graph
+    """
+    self._stylizers['edge_shapes'] = GraphEdgeShaper.build(*args, **kwargs)
+
+    self._engine_tick()
+
+
+  def scale_edges_by(self, *args, **kwargs):
+    """
+    todo: implement this
+    """
+
+
+  def label_edges_by(self, *args, **kwargs):
+    """
+    Creates an instance of GraphEdgeLabeler used by this graph
+    """
+    self._stylizers['edge_labels'] = GraphEdgeLabeler.build(*args, **kwargs)
+
+    self._engine_tick()
+
+
+  def _default_stylizers(self):
+    """
+    Creates and saves the default stylizers.
+    """
+    self._stylizers = { # pylint: disable=attribute-defined-outside-init
+      'node_colors': GraphNodeColorizer.build(lambda u, G: None),
+      'node_shapes': GraphNodeShaper.build(lambda u, G: None),
+      'node_scales': None,
+      'node_labels': GraphNodeLabeler.build(lambda u, G: None),
+
+      'edge_colors': GraphEdgeColorizer.build(lambda u, v, G: None),
+      'edge_shapes': GraphEdgeShaper.build(lambda u, v, G: None),
+      'edge_scales': None,
+      'edge_labels': GraphEdgeLabeler.build(lambda u, v, G: None),
+    }
--- src/engine/graph/di_graph.py
+++ src/engine/graph/di_graph.py
@@ -1,14 +1,14 @@
 """
-The Graph Visualizer module
+The DiGraph Visualizer module
 """
 
 import networkx
 from .base_graph import BaseGraph
 
 
-class Graph(networkx.Graph, BaseGraph):
+class DiGraph(networkx.DiGraph, BaseGraph):
   """
-  Graph Visualizer is simply a wrapper for the networkx.Graph class which
+  DiGraph Visualizer is simply a wrapper for the networkx.DiGraph class which
   implements all visualization methods. The more precise description can be
   found in the following document:
     - https://syga.kam.mff.cuni.cz/docs/graphs/graphs
@@ -18,13 +18,13 @@ class Graph(networkx.Graph, BaseGraph):
     """
     Returns the graph type.
     """
-    return 'Graph'
+    return 'DiGraph'
 
 
   def __init__(self, incoming_graph_data=None, **attr):
     """
-    Creates a new instance of Graph. A new instance MUST be creating by calling
-    engine.Graph(), which calls this constructor and forwards the arguments
+    Creates a new instance of DiGraph. A new instance MUST be creating by calling
+    engine.DiGraph(), which calls this constructor and forwards the arguments
     """
     super().__init__(incoming_graph_data=incoming_graph_data, **attr)
 
