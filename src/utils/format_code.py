3a792d963d53492000bc983b1d7351e912cc47d3
telefon <kheltan.engineering@gmail.com>
2026-03-05 11:39:04 +0100

    fix: longer line length for syga formatter

--- src/utils/format_code.py
+++ src/utils/format_code.py
@@ -4,7 +4,7 @@ import black
 def format_code(code: str) -> str:
     try:
         # Format with Black
-        formatted = black.format_str(code, mode=black.FileMode(line_length=40))
+        formatted = black.format_str(code, mode=black.FileMode(line_length=60))
         return formatted
     except Exception as e:
         raise e
